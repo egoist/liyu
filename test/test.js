b3847f96c428e1e097a55a4e06bf82f72177b77d
Termux on EGOIST's One Plus 6 <0x142857@gmail.com>
2018-12-30 03:35:09 +0800

    tweaks

--- test/test.js
+++ test/test.js
@@ -3,5 +3,5 @@ import liyu from '../lib'
 
 test('main', async t => {
   const res = await liyu('abc', {limit: 1})
-  t.is(res[0].definition, 'American Born Chinese')
+  t.is(res[0].definition, '[American] [Born] [Chinese]')
 })
